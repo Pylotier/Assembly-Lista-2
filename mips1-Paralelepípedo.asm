3336101ae4137184e80ed900320a45aa2c85ff60
Pylotier <marcosmario2011@gmail.com>
2026-05-01 11:22:01 -0300

    Update mips1-Paralelepípedo.asm

--- mips1-Paralelepípedo.asm
+++ mips1-Paralelepípedo.asm
@@ -1,34 +1,45 @@
 .data
 	msg01: .asciiz"Digite a lagura do paralelepípedo: "
 	msg02: .asciiz"Digite a altura do paralelepípedo: "
-	msg03: .asciiz"A área do paralelepípedo é: "
+	msg03: .asciiz"Digite a profundidade paralelepípedo: : "
+	msg04: .asciiz"O volume do paralelepípedo é: "
 .text
 .main
 	li $v0, 4
 	la $a0, msg01
-	syscall
+	syscall # Perguntar qual a largura
 	
 	li $v0, 5
-	syscall
+	syscall # Esperar input
 	
-	add $t1, $v0, 0
+	add $t1, $v0, 0 # Guaradar valor da altura
 	
 	li $v0, 4
-	la $a0, msg02
-	syscall
+	la $a0, msg02 
+	syscall # Perguntar qual a altura
 	
 	li $v0, 5
-	syscall
+	syscall # Esperar input
 	
-	add $t2, $v0, 0
-	
-	mul $s1, $t1, $t2
+	add $t2, $v0, 0 # Guaradar valor da largura
 	
 	li $v0, 4
-	la $a0, msg03
-	syscall
+	la $a0, msg03 
+	syscall # Perguntar qual a profundidade
+	
+	li $v0, 5
+	syscall # Esperar input
+	
+	add $t3, $v0, 0 # Guaradar valor da profundidade
+	
+	# V = a.b.c
+	mul $s1, $t1, $t2 # Calculo de área R1 = (L*A)
+	mul $s2, $s1, $t3 # Calculo de área (R*P)
+	
+	li $v0, 4
+	la $a0, msg04
+	syscall # Falar a área
 	
 	li $v0, 1
-	add $a0, $s1, 0
-	syscall
-	
+	add $a0, $s2, 0
+	syscall # Falar a área do paralelepípedo
